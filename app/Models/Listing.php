ca7fba395c3ab85b5707d3ebcde95b22469f3b4a
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-08-07 10:37:03 +0100

    added relationships

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Listing extends Model
 {
     use HasFactory;
+
+    // Relationship To User
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
