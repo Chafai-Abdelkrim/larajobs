c5ed0b0f1b94fcde80247de236c2becd5099acad
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2023-08-07 09:03:07 +0100

    added user registeration

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\ListingController;
 use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +30,8 @@ Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+Route::get('/register', [UserController::class, 'create']);
+
+Route::post('/users', [UserController::class, 'store']);
+
